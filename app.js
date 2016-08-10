725f2628ff2fb77fad24e576085afff2ef45849b
boots525 <alexisalawson@gmail.com>
2016-08-09 22:19:35 -0400

    second commit

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 //create ipsum//
 function millipsumBank(elem){
 	var millipsum = new Array("duh", 
-	"omg", "bae", "netflix and chill", "pokemon go", "bernie", "the struggle", "AF", "literally", "10/10", "tho",
+	"omg", "bae", "netflix and chill?", "pokemon go", "bernie", "the struggle.", "AF", "literally.", "10/10", "tho",
 	 "said no one ever", "I can't even", "#sorrynotsorry", "haters", "this.", "bro", "white girl", "first world probs", 
-	 "rachet", "turnt", "lulz", "burn", "foolishness", "basic", "thirsty", "THOT", "on fleek", "it me", "fam", "no chill", "epic", "for reals", "turn down for what", 
-	 "throw shade", "yasss", "bye felicia", "squad", "#goals", "cray", "hella", "flex", "mcm", "the feels", "swerve", "woke", "doe", "amirite?", "keep it 100", "tbh", "dat", "shots fired", 
-	 "lit", "swipe right", "low key", "savage", "smh", "fail", "win", "lol" ); 
+	 "rachet", "turnt", "lulz.", "burn", "foolishness", "basic", "thirsty", "THOT", "on fleek.", "it me", "fam", "no chill", "epic", "for reals", "turn down for what", 
+	 "throw shade", "yasss", "bye felicia", "squad", "#goals", "cray", "hella", "flex", "mcm", "the feels", "swerve.", "woke.", "doe", "amirite?", "keep it 100", "tbh", "dat", "shots fired", 
+	 "lit", "swipe right", "low key", "savage", "smh", "fail", "win", "lol", "you need to stop", "ICYMI", "TL;DR", "catfish", "slay", "facepalm", "swag")
 
-	var minWord = 100; 
+	var minWord = 75; 
 	var maxWord = 150; 
 
 	var random = Math.floor(Math.random()**(maxWord - minWord)) + minWord; 
